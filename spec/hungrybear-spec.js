55480228193fc043e0c5ab99e02f770c526bc684
Kira Roberts and Mike Lofgren <kira@>
2019-07-01 14:56:40 -0700

    Add initial files, logic, specs

---
+++ spec/hungrybear-spec.js
@@ -0,0 +1,71 @@
+import { bear } from '../src/hungrybear.js';
+
+describe('HungryBear', function() {
+  let fuzzy = bear;
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    fuzzy.foodLevel = 10;
+    fuzzy.name = "Fuzzy";
+    fuzzy.rageLevel = 0
+    fuzzy.sleepiness = 0;
+    fuzzy.setHunger();
+    fuzzy.enrage();
+    fuzzy.goingToSleep();
+    // fuzzy.setRampageMeter();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('should have a name and a food level of 10 when it is created', function() {
+    expect(fuzzy.name).toEqual("Fuzzy");
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
+  it('should have a food level of 7 after 3001 milliseconds', function() {
+    jasmine.clock().tick(3001);
+    expect(fuzzy.foodLevel).toEqual(7);
+  });
+
+  it('should get very hungry if the food level drops below zero', function() {
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should get very hungry if 10 seconds pass without feeding', function() {
+    jasmine.clock().tick(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should return that the bear ate blueberries and the food level should go up 5', function() {
+    expect(fuzzy.eatSmall("blueberries")).toEqual("The bear ate the blueberries! Food level goes up by 5!");
+    expect(fuzzy.foodLevel).toEqual(15);
+  });
+
+  // it('should get mauled if rage meter over 40', function() {
+  //   jasmine.clock().tick(40001);
+  //   expect(fuzzy.didYouGetMauled()).toEqual(true);
+  // });
+
+  it('should go to sleep if sleepiness is over 15', function () {
+    fuzzy.sleepiness = 16
+    expect(fuzzy.goingToSleep()).toEqual(true);
+  });
+
+
+
+  // it('should go to sleep if bears sleepiness is over15 ', function() {
+  //     fuzzy.hibernate()
+  //   jasmine.clock().tick(15001);
+  //   console.log(fuzzy.sleepiness)
+  //   expect(fuzzy.goingToSleep()).toEqual(true);
+  // });
+  //
+  // it ('should wake you up if ')
+  // it('should maul you if the rage meter is 25 or above', function() {
+    // jasmine.clock().tick(1)
+  // });
+
+});
---
+++ src/hungrybear.js
@@ -0,0 +1,151 @@
+export let bear = {
+  foodLevel: 10,
+  rageLevel: 0,
+  sleepiness: 0,
+  setHunger: function() {
+    const hungerInterval = setInterval(() => {
+      this.foodLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(hungerInterval);
+        return "You got eaten!";
+      }
+    }, 1000);
+  },
+  didYouGetEaten: function() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  feed: function(amount) {
+    let that = this;
+    return function(food) {
+      that.foodLevel += amount
+      return `The bear ate the ${food}! Food level goes up by ${amount}!`
+    }
+  },
+  enrage: function() {
+    const rageInterval = setInterval(() => {
+      this.rageLevel += 1;
+      if (this.didYouGetMauled() == true) {
+        clearInterval(rageInterval);
+        return "You got mauled!";
+      }
+    }, 1000);
+  },
+  didYouGetMauled: function() {
+    if (this.rageLevel < 40) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  hibernate: function() {
+    console.log("mike is a dickhead")
+    const sleepInterval = setInterval(() => {
+      this.sleepinees ++;
+      if (this.goingToSleep() == true) {
+        clearInterval(sleepInterval);
+        return "ZZZzzzzzzz";
+      }
+    }, 1000);
+  },
+  goingToSleep: function() {
+    if (this.sleepiness > 15) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+bear.eatSmall = bear.feed(5);
+bear.eatMedium = bear.feed(10);
+bear.eatLarge = bear.feed(15);
+bear.eatYuck = bear.feed(-10);
+bear.eatPowerUp = bear.feed(50);
+bear.eatSpecialBonus = bear.feed(100);
+bear.eatWeirdThing = bear.feed(Math.floor((Math.random() * 20) + 1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   constructor(name) {
+  //     this.name = name;
+  //     this.foodLevel = 10;
+  //     this.rageMeter = 0;
+  //   }
+  //
+  //   setHunger() {
+  //     setInterval(() => {
+  //       this.foodLevel--;
+  //     }, 1000);
+  //   }
+  //
+  //   setRampageMeter() {
+  //     setInterval(() => {
+  //       this.rageMeter ++;
+  //     }, 1000);
+  //   }
+  //
+  //   didYouGetMauled() {
+  //     if (this.rageMeter >= 40) {
+  //       return true
+  //     } else {
+  //       return false
+  //     }
+  //   }
+  //
+  //   digThroughTrash () {
+  //     if (this.rageMeter >= 25) {
+  //       this.foodLevel -= 3
+  //     }
+  //
+  //   // didYouWakeUp() {
+  //   //   if (this.rageMeter >= 20) {
+  //   //     return true;
+  //   //   } else {
+  //   //     return false;
+  //   //   }
+  //   // }
+  //
+  //   didYouGetEaten() {
+  //     if (this.foodLevel > 0) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   }
+  //
+  //
+  //
+  //   feed() {
+  //     this.foodLevel = 10;
+  //   }
+  // }
